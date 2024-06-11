e7b0f2dbd6cc59a4ca8d13e22fdc12ecc605d227
Jesús González <gonzalez.jesus@safetwice.com>
2024-06-11 11:34:42 +0200

    Add SlidingToolbar and ContentControlWithToolbar

---
+++ Lib/ContentControlWithToolbar/ContentControlWithToolbar.cs
@@ -0,0 +1,111 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Utilities.DotNet.WPF.Controls
+{
+    /// <summary>
+    /// <see cref="ContentControl"> with an overlaid <see cref="SlidingToolbar"/> than can be visible or hidden.
+    /// </summary>
+    [TemplatePart( Name = "PART_Toolbar", Type = typeof( SlidingToolbar ) )]
+    public class ContentControlWithToolbar : HeaderedContentControl
+    {
+        //===========================================================================
+        //                           PUBLIC PROPERTIES
+        //===========================================================================
+
+        public static readonly DependencyProperty IsHeaderVisibleProperty =
+            DependencyProperty.Register( nameof( IsHeaderVisible ), typeof( bool ), typeof( ContentControlWithToolbar ),
+                new FrameworkPropertyMetadata( false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnIsHeaderVisibleChanged ) );
+
+        [Bindable( true )]
+        [Browsable( true )]
+        public bool IsHeaderVisible
+        {
+            get => (bool) GetValue( IsHeaderVisibleProperty );
+            set => SetValue( IsHeaderVisibleProperty, value );
+        }
+
+        public static readonly DependencyProperty HeaderMarginProperty =
+            DependencyProperty.Register( nameof( HeaderMargin ), typeof( Thickness ), typeof( ContentControlWithToolbar ),
+                new FrameworkPropertyMetadata( new Thickness() ) );
+
+        [Bindable( true )]
+        [Browsable( true )]
+        public Thickness HeaderMargin
+        {
+            get => (Thickness) GetValue( HeaderMarginProperty );
+            set => SetValue( HeaderMarginProperty, value );
+        }
+
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        static ContentControlWithToolbar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata( typeof( ContentControlWithToolbar ),
+                new FrameworkPropertyMetadata( typeof( ContentControlWithToolbar ) ) );
+        }
+
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if( m_toolbar != null )
+            {
+                m_toolbar.IsVisibleChanged -= SlidingToolbar_OnIsVisibleChanged;
+            }
+
+            m_toolbar = GetTemplateChild( "PART_Toolbar" ) as SlidingToolbar;
+            
+            if( m_toolbar != null )
+            {
+                OnIsHeaderVisibleChanged( IsHeaderVisible );
+                m_toolbar.IsVisibleChanged += SlidingToolbar_OnIsVisibleChanged;
+            }
+        }
+
+        private static void OnIsHeaderVisibleChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
+        {
+            if( d is ContentControlWithToolbar control )
+            {
+                control.OnIsHeaderVisibleChanged( (bool) e.NewValue );
+            }
+        }
+
+        private void OnIsHeaderVisibleChanged( bool newValue )
+        {
+            if( m_toolbar != null )
+            {
+                m_toolbar.Visibility = newValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private void SlidingToolbar_OnIsVisibleChanged( object sender, DependencyPropertyChangedEventArgs e )
+        {
+            if( sender is SlidingToolbar slidingToolbar )
+            {
+                IsHeaderVisible = slidingToolbar.IsVisible;
+            }
+        }
+
+        //===========================================================================
+        //                            PRIVATE METHODS
+        //===========================================================================
+
+        //===========================================================================
+        //                           PRIVATE ATTRIBUTES
+        //===========================================================================
+
+        private SlidingToolbar? m_toolbar;
+    }
+}
---
+++ Lib/SlidingToolbar/SlidingToolbar.cs
@@ -0,0 +1,97 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Utilities.DotNet.WPF.Controls
+{
+    /// <summary>
+    /// Toolbar that can be hidden and resized horizontally.
+    /// </summary>
+    [TemplatePart( Name = "PART_CloseButton", Type = typeof( Button ) )]
+    public class SlidingToolbar : ContentControl
+    {
+        //===========================================================================
+        //                           PUBLIC PROPERTIES
+        //===========================================================================
+
+        public static readonly DependencyProperty IsCloseButtonVisibleProperty =
+            DependencyProperty.Register( nameof( IsCloseButtonVisible ), typeof( bool ), typeof( SlidingToolbar ),
+                new FrameworkPropertyMetadata( true, OnIsCloseButtonVisibleChanged ) );
+
+        [Bindable( true )]
+        [Browsable( true )]
+        public bool IsCloseButtonVisible
+        {
+            get => (bool) GetValue( IsCloseButtonVisibleProperty );
+            set => SetValue( IsCloseButtonVisibleProperty, value );
+        }
+
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        static SlidingToolbar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata( typeof( SlidingToolbar ), new FrameworkPropertyMetadata( typeof( SlidingToolbar ) ) );
+
+            VisibilityProperty.OverrideMetadata( typeof( SlidingToolbar ), new FrameworkPropertyMetadata( Visibility.Visible, 
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault ) );
+        }
+
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <inheritdoc/>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if( m_closeButton != null )
+            {
+                m_closeButton.Click -= CloseButton_OnClick;
+            }
+
+            m_closeButton = GetTemplateChild( "PART_CloseButton" ) as Button;
+
+            if( m_closeButton != null )
+            {
+                m_closeButton.Click += CloseButton_OnClick;
+
+                OnIsCloseButtonVisibleChanged( IsCloseButtonVisible );
+            }
+        }
+
+        //===========================================================================
+        //                            PRIVATE METHODS
+        //===========================================================================
+
+        private void CloseButton_OnClick( object sender, RoutedEventArgs e )
+        {
+            Visibility = Visibility.Hidden;
+        }
+
+        private static void OnIsCloseButtonVisibleChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
+        {
+            ( d as SlidingToolbar )?.OnIsCloseButtonVisibleChanged( (bool) e.NewValue );
+        }
+
+        private void OnIsCloseButtonVisibleChanged( bool newValue )
+        {
+            if( m_closeButton != null )
+            {
+                m_closeButton.Visibility = newValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        //===========================================================================
+        //                           PRIVATE ATTRIBUTES
+        //===========================================================================
+
+        private Button? m_closeButton;
+    }
+}
